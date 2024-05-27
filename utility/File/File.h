bf3a85bef7f804980b647a8f572a4a348259b9b3
leo <wangsihan_leo@sjtu.edu.cn>
2024-05-28 07:04:09 +0800

    BugFix: minimize buffer pool size

--- utility/File/File.h
+++ utility/File/File.h
@@ -61,7 +61,7 @@ class Storage {
   void Clear();
 
  private:
-  static constexpr int BufferPoolSize = 1024;
+  static constexpr int BufferPoolSize = 512;
   FileWithInt<T, len> data_file_;
   FileWithInt<int> trash_file_;
   LRU<T, len> *buffer;
